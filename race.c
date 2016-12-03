2af891e8f411c3f8851721df1fe29e381ccc7bf9
dseekatz <dseekatz@gmail.com>
2016-12-03 11:35:24 -0500

    Rough outline.  Currently prints starting positions without lane numbers.

--- race.c
+++ race.c
@@ -2,21 +2,53 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<pthread.h>
+#include<string.h>
 
-#define NUM_THREADS 5
+#define NUM_AI 4
 
-void *carAI(void *threadid) {
+void *carAI(void *aString) {
+	char* str;
+	str = (char*) aString;
+	printf("%s\n", str);
 	// Code for AI cars
 	pthread_exit(NULL);
 } // end carAI
 
-void *carUser(void *threadid) {
+void *carUser(void *aString) {
+	char* str;
+	str = (char*) aString;
+	printf("%s\n", str);
 	// Code for user car
 	pthread_exit(NULL);
 } // end carUser
 
 int main() {
+	// Initialize threads and other variables
+	pthread_t threads[NUM_AI + 1];
+	int i;
+	int errorCheck;
+	char str[54];
+	strcpy(str, "|->                                        # Lane ");
 	
+	// Create user thread	
+	printf("In main: creating user thread\n");
+	errorCheck = pthread_create(threads, NULL, carUser, (void *) str);
+	if (errorCheck) {
+		printf("ERROR: Return code from pthread_create() is %d\n", errorCheck);
+		exit(-1);
+	} // end conditional
+	
+	// Create AI threads
+	for (i = 0; i < NUM_AI; i++) {
+		printf("In main: creating an AI thread\n");
+		errorCheck = pthread_create(threads, NULL, carAI, (void *) str);
+		if (errorCheck) {
+			printf("ERROR: Return code from pthread_create() is %d\n", errorCheck);
+			exit(-1);
+		} // end conditional
+	} // end for loop	
+	
+	// Close everything and exit
 	pthread_exit(NULL);
 	return 0;
 } // end main
