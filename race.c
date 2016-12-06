f5c18d1e5e35b25186e2e6a0c2145c2551cfce58
dseekatz <dseekatz@gmail.com>
2016-12-06 16:37:49 -0500

    Added Simons stuff to race.c ... player 5 still always wins, unless the program is run using the debugger, in which case it works sort of properly.

--- race.c
+++ race.c
@@ -17,12 +17,12 @@ void *carAI(void *args) {
 	srand((unsigned) time(&t));
 	while (1) {
 		if (AIcounters[carAINums] == 40) {
-			printf("Player %d Wins!",carAINums+1);
+			printf("Player %d Wins!\n",carAINums+1);
 			exit(0);
 		} // end conditional
 		AIcounters[carAINums]++;
 		int waitTime = rand() % 100;
-		usleep(waitTime*1000);
+		usleep(waitTime*10000);
 	} // end while loop
 	pthread_exit(NULL);
 } // end carAI
@@ -33,17 +33,69 @@ void *carUser(void *args) {
 	pthread_exit(NULL);
 } // end carUser
 
+void printstuff(int progress, int laneNum) {
+	
+	char carNumber[4];
+	sprintf(carNumber,"%d #",laneNum);
+	char lane[54] = "# Lane ";
+	char aCar[] = "|->";
+	char travel[] = "~";
+	char blank[54] = "";
+	char distance[54] = "";
+
+	for (int i = 0 ; i < progress ; i++) {
+		strcat(blank, travel);
+	}
+
+	for (int i = 0; i < 40 - progress ; i++) {
+		strcat(distance, " ");
+	}
+	strcat(blank, aCar);
+	strcat(blank, distance);
+	strcat(blank, lane);
+	strcat(blank, carNumber);
+	printf("\n%s\n", blank);
+}
+
 // Draw function
 void *draw(void *args) {
-	// Code for drawing thread
+	int i = 0;
+	while(1) {
+		printstuff(user, 1);
+		for (i = 2; i < NUM_AI + 2; i++) {
+			printstuff(AIcounters[i-2], i);
+		} // end for loop
+		usleep(20000);
+		system("clear");
+	} // end while loop
 	pthread_exit(NULL);
 } // end draw
 
 int main() {
+	// print starting conditions
+	system("clear");
+	printf("Welcome to CISC220 Racing Arena!\nHit enter to move forward\n");
+	printstuff(39, 1);
+	int i;
+	for (i = 2; i < NUM_AI + 2; i++) {
+		printstuff(AIcounters[i-2], i);
+	} // end for loop
+
 	// Initialize threads and other variables
 	pthread_t threads[NUM_AI + 2];
-	int i = 0;
 	int errorCheck;
+
+	// Make sure the user starts the game
+	char hitEnter;
+	scanf("%c",&hitEnter);
+
+	// Create drawing thread	
+	printf("In main: creating drawing thread\n");
+	errorCheck = pthread_create(threads, NULL, draw, (void *) &i);
+	if (errorCheck) {
+		printf("ERROR: Return code from pthread_create() is %d\n", errorCheck);
+		exit(-1);
+	} // end conditional
 
 	// Create user thread	
 	printf("In main: creating user thread\n");
@@ -62,14 +114,6 @@ int main() {
 			exit(-1);
 		} // end conditional
 	} // end for loop	
-
-	// Create drawing thread	
-	printf("In main: creating drawing thread\n");
-	errorCheck = pthread_create(threads, NULL, draw, (void *) &i);
-	if (errorCheck) {
-		printf("ERROR: Return code from pthread_create() is %d\n", errorCheck);
-		exit(-1);
-	} // end conditional
 
 	// Close everything and exit
 	pthread_exit(NULL);
